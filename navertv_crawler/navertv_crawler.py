b05fcfef5b287cfbe1830d542add7b543603deb8
beoms0 <bumsookim98@naver.com>
2018-11-30 04:31:31 +0900

    b

--- navertv_crawler/navertv_crawler.py
+++ navertv_crawler/navertv_crawler.py
@@ -16,6 +16,10 @@ he_name = "전현무"
 # <span class="count _commentCount">30</span>
 #<em class="u_cnt _cnt">334</em>
 # tit_list = soup.find_all('div', class_ = 'tit_inner')("//div[@class='alex_more']")
+
+list_rank = 2
+
+#데이터 수집
 heart_count = driver.find_element_by_xpath("//em[@class = 'u_cnt _cnt']").text
 reply_count = driver.find_element_by_xpath("//span[@class = 'count _commentCount']").text
 play_count = driver.find_element_by_xpath("//span[@class = 'play']").text
@@ -24,14 +28,26 @@ reply_list = []
 replies = driver.find_elements_by_xpath("//span[@class = 'u_cbox_contents']")
 for a in replies:
     reply_list.append(a.text)
-playlisttitle = driver.find_element_by_xpath("//span[@class = '_playlistTitle']").text
 
-print(heart_count)
-print(reply_count)
-print(play_count)
-print(is_he)
-print(reply_list)
-print(playlisttitle)
+#같은 회차 내 다음 영상으로 이동
+click_path = "//*[@id='playlistClip']/li[{}]/div/dl/dt/a".format(list_rank)
+click_element = driver.find_element_by_xpath(click_path)
+click_element.click()
+time.sleep(3)
+
+#다음 재생목록으로 이동
+click_path2 = //*[@id="playlistClipScrollBox"]/div[1]/div/div/ul/li[2]/a
+click_element2 = driver.find_element_by_xpath(click_path2)
+click_element2.click()
+time.sleep(3)
+
+
+# print(heart_count)
+# print(reply_count)
+# print(play_count)
+# print(is_he)
+# print(reply_list)
+# print(playlisttitle)
 
 
 
